0cf70d8f3072f132223e48b71a2aba5da96b6dcb
Merrick Kavolsky <technicmk@gmail.com>
2017-02-04 21:21:37 +0200

    Update dummy codeception test

--- tests/api/DummyCept.php
+++ tests/api/DummyCept.php
@@ -1,5 +1,8 @@
 <?php 
 $I = new ApiTester($scenario);
 $I->wantTo('perform actions and see result');
-$I->sendGET('');
-$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
+$I->sendPOST('users/', ['email' => 'angela@moss.com', 'password' => 'string-beans']);
+$I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED); // 200
+$I->sendGET('users/');
+
+$I->seeResponseMatchesJsonType(['email' => 'string', 'id' => 'integer'], '$.data[0]');
