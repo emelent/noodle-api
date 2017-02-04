8ca8547b076eed0462b39d4662d3e8646ba194fc
Merrick Kavolsky <technicmk@gmail.com>
2017-02-04 21:12:30 +0200

    Add user routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+$app->group([
+  'prefix'  =>  '/api/v1/',
+], function() use ($app){
+
+  $app->get('/', function() use($app){
+    return response()->json(['data' => "Looks like you're well ReSted"]);
+  });
+
+
+  /*user routes*/
+  $app->group([
+    'prefix'  =>  'users/',
+  ], function() use ($app){
+    $app->get('/', 'UserController@index');
+    $app->post('/', 'UserController@store');
+    $app->get('/{user_id}', 'UserController@show');
+    $app->put('/{user_id}', 'UserController@update');
+    $app->delete('/{user_id}', 'UserController@destroy');
+  });
+});
