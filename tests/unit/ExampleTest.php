f7fd709925d3c4e3151e08cc0dda0be8352dda6d
Merrick Kavolsky <technicmk@gmail.com>
2017-02-04 21:24:50 +0200

    Update tests

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+use App\User;
+
+class ExampleTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+      $user = User
+    }
+}
