b1d5515c5aea8cdb9cfc110cb43129a4983dfdc6
Merrick Kavolsky <technicmk@gmail.com>
2017-02-04 21:14:49 +0200

    Fix bug with fillable and hidden attributes

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+      'email', 'password'
     ];
 
     /**
@@ -27,6 +27,6 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password',
+        'password', 'created_at', 'updated_at'
     ];
 }
