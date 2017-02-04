c2daf83da61d89632a2c6ebd48f5b5f42547485d
Merrick Kavolsky <technicmk@gmail.com>
2017-02-04 21:16:38 +0200

    Update base controller, use status code constants

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Lukasoppermann\Httpstatus\Httpstatuscodes;
 
-class Controller extends BaseController
+class Controller extends BaseController implements Httpstatuscodes
 {
-    //
+  public function success($data, $code){
+    return response()->json(['data' => $data], $code);
+  } 
+
+  public function error($message, $code){
+    return response()->json(['message' => $message, $code]);
+  }
 }
