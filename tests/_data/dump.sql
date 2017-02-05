29d24a2576ce1846aaa981469f52ba1692820144
Merrick Kavolsky <technicmk@gmail.com>
2017-02-05 02:39:30 +0200

    Add database dsn to codeception config

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,6 @@
-/* Replace this file with actual dump of your database */
+BEGIN TRANSACTION;
+CREATE TABLE "users" ("id" integer not null primary key autoincrement, "email" varchar not null, "password" varchar not null, "created_at" datetime null, "updated_at" datetime null);
+CREATE TABLE "migrations" ("id" integer not null primary key autoincrement, "migration" varchar not null, "batch" integer not null);
+INSERT INTO `migrations` VALUES (16,'2017_02_04_142210_create_users_table',1);
+CREATE UNIQUE INDEX "users_email_unique" on "users" ("email");
+COMMIT;
