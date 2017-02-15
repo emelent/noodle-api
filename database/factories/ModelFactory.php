04bfcdd1420eb20171dd13915e7277f461a28795
Merrick Kavolsky <technicmk@gmail.com>
2017-02-16 01:41:27 +0200

    Create Event model factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -18,3 +18,17 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     'password' => $hasher->make('secret')
   ];
 });
+
+$factory->define(App\Event::class, function (Faker\Generator $faker) {
+  return [
+    'day' => rand(0,6),
+    'language'  => rand(0, 3),
+    'group' => rand(0, 6),
+    'creator_id'  => rand(0, 10),
+    'module_id' => rand(0, 10),
+    'name'  => $faker->bothify('???### Lesson #'),
+    'date' => $faker->date(),
+    'start'  => $faker->time(),
+    'end'  => $faker->time()
+  ];
+});
