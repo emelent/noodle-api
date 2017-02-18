11fb1c7123d4cadd215eaacd3e752820a58a1bce
Merrick Kavolsky <technicmk@gmail.com>
2017-02-18 07:52:51 +0200

    Add model factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,6 +11,7 @@
 |
 */
 
+//User Model factory
 $factory->define(App\User::class, function (Faker\Generator $faker) {
   $hasher = app()->make('hash');
   return [
@@ -19,16 +20,35 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
   ];
 });
 
+//Event Model factory
 $factory->define(App\Event::class, function (Faker\Generator $faker) {
   return [
     'day' => rand(0,6),
     'language'  => rand(0, 3),
-    'group' => rand(0, 6),
-    'creator_id'  => rand(0, 10),
-    'module_id' => rand(0, 10),
+    'group' => rand(1, 6),
+    'creator_id'  => rand(1, 10),
+    'module_id' => rand(1, 10),
     'name'  => $faker->bothify('???### Lesson #'),
     'date' => $faker->date(),
     'start'  => $faker->time(),
     'end'  => $faker->time()
+  ];
+});
+
+//Module Model factory
+$factory->define(App\Module::class, function(Faker\Generator $faker){
+  return [
+    'name'  => $faker->word,
+    'code'  => $faker->bothify('???###'),
+    'description' => $faker->realText(),
+    'type'  => rand(0,2),
+    'period'  => $faker->randomElement(['S1','S2','Q1','Q2','Q3','Q4'])
+  ];
+});
+
+//Timetable Model factory
+$factory->define(App\Timetable::class, function(Faker\Generator $faker){
+  return [
+    'creator_id'  => rand(1,10)
   ];
 });
