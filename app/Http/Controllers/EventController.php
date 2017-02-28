bcc1186a2c4452e614e4b284af744a3982231d34
Merrick Kavolsky <technicmk@gmail.com>
2017-02-28 18:19:02 +0200

    DRY up Event controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,6 +7,30 @@ use Illuminate\Http\Request;
 
 class EventController extends ModelController{
 
+	protected $storeRules = [
+		'name'	=> 'required|string',
+		'day'		=> 'required|integer|between:1,7',
+		'start'	=> 'required|regex:/^(\d{2})(:)(\d{2})$/',
+		'end'	=> 'required|regex:/^(\d{2})(:)(\d{2})$/',
+		'date'	=> 'required|date',
+		'language'	=> 'required|integer',
+		'creator_id'	=> 'required|integer',
+		'module_id'	=> 'required|integer',
+		'group'	=> 'integer'
+	];
+
+	protected $updateRules = [
+		'name'	=> 'string',
+		'day'		=> 'integer|between:1,7',
+		'start'	=> 'regex:/^(\d{2})(:)(\d{2})$/',
+		'end'	=> 'regex:/^(\d{2})(:)(\d{2})$/',
+		'date'	=> 'date',
+		'language'	=> 'integer',
+		'creator_id'	=> 'integer',
+		'module_id'	=> 'integer',
+		'group'	=> 'integer',
+	];
+
   public function __construct(){
     parent::__construct(Event::class);
     $this->middleware('auth:api', ['except' => [
@@ -14,52 +38,27 @@ class EventController extends ModelController{
     ]]);
   }
 
-	public function store(Request $request){
-		$this->validateRequest($request);
-
-		$event = Event::create([
-      'name' => $request->get('name'),
-      'date' => $request->get('date'),
-      'day' => $request->get('day'),
-      'start' => $request->get('start'),
-      'end' => $request->get('end'),
-      'language' => $request->get('language'),
-      'group' => $request->get('group'),
-      'creator_id' => $request->user()->id,
-      'module_id' => $request->get('module_id'),
-    ]);
-
-		return $this->success("The event has been created.", self::HTTP_CREATED);
-	}
 
 	public function update(Request $request, $id){
 		$event = Event::find($id);
 		if(!$event){
-			return $this->error("The event with {$id} doesn't exist", self::HTTP_NOT_FOUND);
+			return $this->error("The event with $id doesn't exist", self::HTTP_NOT_FOUND);
 		}
-
-		if(!$event->creator_id != $request->user()->id){
-			return $this->error("Unauthorized.", self::HTTP_UNAUTHORIZED);
+		if($event->creator_id != $request->user()->id){
+			return $this->error("Not permitted.", self::HTTP_UNAUTHORIZED);
 		}
-
-		$this->validateRequest($request);
-		$event->name = $request->get('name');
-		$event->date = $request->get('date');
-		$event->start = $request->get('start');
-		$event->end = $request->get('end');
-		$event->date = $request->get('date');
-		$event->language = $request->get('language');
-		$event->group = $request->get('group');
-		$event->module_id = $request->get('module_id');
-		$event->save();
-		return $this->success("The event with id {$event->id} has been updated.", self::HTTP_OK);
+		parent::update($request, $id);
 	}
 
-	public function validateRequest(Request $request){
-		//$rules = [
-			//'email' => 'required|email|unique:events', 
-			//'password' => 'required|min:6'
-		//];
-		//$this->validate($request, $rules);
+
+	public function destroy(Request $request, $id){
+		$event = Event::find($id);
+		if(!$event){
+			return $this->error("The event with $id doesn't exist", self::HTTP_NOT_FOUND);
+		}
+		if($event->creator_id != $request->user()->id){
+			return $this->error("Not permitted.", self::HTTP_UNAUTHORIZED);
+		}
+		parent::destroy($request, $id);
 	}
 }
