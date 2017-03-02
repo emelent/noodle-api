6935df567f878eb87aff0e691e394ab01cba431c
Merrick Kavolsky <technicmk@gmail.com>
2017-03-02 23:21:35 +0200

    Update Timetable controller to comply with tests

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -7,18 +7,19 @@ use Illuminate\Http\Request;
 
 class TimetableController extends ModelController{
 
-	protected $storeRule = [
+	protected $storeRules = [
 		'creator_id'	=> 'required|integer'
 	];
 
   public function __construct(){
     parent::__construct(Timetable::class);
     $this->middleware('auth:api', ['except'	=> ['show','showAll']]);
-    $this->middleware('role:user', ['except'	=> ['show','showAll']]);
+    // $this->middleware('role:user', ['except'	=> ['show','showAll']]);
   }
 
-	public function update(Request $request, $id){
-    return $this->error('Not found', 404);
+	public function store(Request $request){
+		$request->merge(['creator_id' => $request->user()->id]);
+		return $this->storeAction($request);
 	}
 
 	public function destroy(Request $request, $id){
