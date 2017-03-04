013f4440cabfc00c4a0d381306ed936ef6b4dabe
Merrick Kavolsky <technicmk@gmail.com>
2017-03-04 21:32:51 +0200

    Add helper methods

--- tests/NoAuthModelControllerTestCase.php
+++ tests/NoAuthModelControllerTestCase.php
@@ -3,6 +3,65 @@
 class NoAuthModelControllerTestCase extends TestCase
 {
 	protected $moduleRoutePrefix;
+	protected $modelFields;
+
+  /**
+   * I send a GET request to /api/v1/<modelRoutePrefix>/ and the server
+   * returns a list of models.
+   *
+   * @return void
+   */
+  public function canShowAllModels(){
+    $this->get("{$this->modelRoutePrefix}/")
+      ->seeStatusCode(self::HTTP_OK)
+      ->seeJsonStructure([
+        'data'  => [
+          '*' => $this->modelFields
+        ]
+      ]);
+  }
+
+
+  /**
+   * I send a GET request to  /api/v1/<modelRoutePrefix>/{id} where {id} is a
+   * valid model id and the server returns a list of models. 
+   * 
+   *
+   * @return void
+   */
+  public function canShowModelById(){
+    $id = 1;
+    $this->get("{$this->modelRoutePrefix}/$id")
+      ->seeStatusCode(self::HTTP_OK)
+      ->seeJsonStructure([
+        'data'  => $this->modelFields
+      ]);
+  }
+
+  /**
+   * I send a GET request to /api/v1/<modelRoutePrefix>/ and the server
+   * returns a list of models. (TODO add authentication)
+   *
+   * @return void
+   */
+  public function canNotShowAllModels(){
+    $this->get("{$this->modelRoutePrefix}/")
+      ->seeStatusCode(self::HTTP_UNAUTHORIZED);
+  }
+
+
+  /**
+   * I send a GET request to  /api/v1/<modelRoutePrefix>/{id} where {id} is a
+   * valid model id and the server returns a list of models. 
+   * (TODO add authentication)
+   *
+   * @return void
+   */
+  public function canNotShowModelById(){
+    $id = 1;
+    $this->get("{$this->modelRoutePrefix}/$id")
+      ->seeStatusCode(self::HTTP_UNAUTHORIZED);
+  }
 
 	public function testCanNotCreateNewModel()
 	{
---
+++ tests/api_no_auth/EventControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use App\Event;
+
+class EventControllerTest extends NoAuthModelControllerTestCase
+{
+
+  protected $modelRoutePrefix = '/v1/events';
+  protected $modelFields = [
+    'name', 'day', 'start', 'end',
+    'date', 'language', 'group',
+    'creator_id', 'module_id', 'created_at',
+    'updated_at'
+  ];
+
+  public function testCanShowAllEvents()
+  {
+    $this->canNotShowAllModels();
+  }
+
+  public function testCanShowEventById()
+  {
+    $this->canNotShowModelById();
+  }
+}
