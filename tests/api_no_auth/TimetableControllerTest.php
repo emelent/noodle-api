d6e822f4bcc1ecea239531e90fe696e337b2a7bd
Merrick Kavolsky <technicmk@gmail.com>
2017-03-05 10:28:52 +0200

    Add no_auth timetable controller test

---
+++ tests/api_no_auth/TimetableControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use App\Timetable;
+
+class TimetableControllerTest extends NoAuthModelControllerTestCase
+{
+
+  protected $modelRoutePrefix = '/v1/timetables';
+  protected $modelFields = [
+    'id', 'hash', 'creator_id',
+    'created_at', 'updated_at'
+  ];
+
+  public function testCanShowAllTimetables()
+  {
+    $this->canShowAllModels();
+  }
+
+  public function testCanShowTimetableById()
+  {
+    $this->canShowModelById();
+  }
+}
