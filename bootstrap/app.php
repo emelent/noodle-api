15d60990d5515cded4468799f497746c59f0ec21
Merrick Kavolsky <technicmk@gmail.com>
2017-04-03 17:28:00 +0200

    Configure cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,7 @@ $app = new Laravel\Lumen\Application(
  $app->withFacades();
 
  $app->withEloquent();
+ $app->configure('cors');
 
 /*
 |--------------------------------------------------------------------------
@@ -59,9 +60,9 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+ $app->middleware([
+   \Barryvdh\Cors\HandleCors::class
+ ]);
 
  $app->routeMiddleware([
      'auth' => App\Http\Middleware\Authenticate::class,
@@ -81,7 +82,7 @@ $app->singleton(
 
  $app->register(App\Providers\AppServiceProvider::class);
  $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+ $app->register(Barryvdh\Cors\ServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
